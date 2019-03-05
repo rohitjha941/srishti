01dbf64ac1153ac3e009a965e20fed7f59a4b7f7
Rohit <rohitjha941@hotmail.com>
2019-03-05 21:59:19 +0530

    Team page

--- frontend/src/pages/team/team.jsx
+++ frontend/src/pages/team/team.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./team.scss";
 import Top from "../../static/achievement/top.png";
+import Anant from "../../static/team/anant.1.jpg";
+import Atul from "../../static/team/Atul.1.jpg";
+import Madhur from "../../static/team/madhur.1.jpg";
+import Marcha from "../../static/team/Marcha.jpg";
+import Prasanna from "../../static/team/Prasanna.1.jpg";
 
 export default class Team extends Component {
   render() {
@@ -68,8 +73,7 @@ export default class Team extends Component {
               <p className="name">Prof. Inderdeep Singh</p>
               <p className="des">Coordinator - Tinkering Lab</p>
             </div>
-         
-         
+
             <div className="team_unit">
               <div>
                 <img
@@ -78,10 +82,11 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. Arup Kumar Das</p>
-              <p className="des">Faculty Advisor - SAE, IITR <br /> MotorSports - IITR <br /> Team KNOx</p>
+              <p className="des">
+                Faculty Advisor - SAE, IITR <br /> MotorSports - IITR <br />{" "}
+                Team KNOx
+              </p>
             </div>
-         
-         
 
             <div className="team_unit">
               <div>
@@ -91,7 +96,10 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. Akshay Dvivedi</p>
-              <p className="des">Faculty Advisor - SAE, IITR <br /> MotorSports - IITR <br /> Team KNOx</p>
+              <p className="des">
+                Faculty Advisor - SAE, IITR <br /> MotorSports - IITR <br /> 
+                Team KNOx
+              </p>
             </div>
 
             <div className="team_unit">
@@ -102,9 +110,8 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. G.D. Ransinchung R.N.</p>
-              <p className="des">CED - CEC, IITR</p> 
+              <p className="des">CED - CEC, IITR</p>
             </div>
-         
 
             <div className="team_unit">
               <div>
@@ -114,9 +121,9 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. V Huzur Saran</p>
-              <p className="des">Coordinator - ASME</p> 
+              <p className="des">Coordinator - ASME</p>
             </div>
-         
+
             <div className="team_unit">
               <div>
                 <img
@@ -125,9 +132,8 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. P. Sateesh Kumar</p>
-              <p className="des">Coordinator - ACM</p> 
+              <p className="des">Coordinator - ACM</p>
             </div>
-
 
             <div className="team_unit">
               <div>
@@ -137,26 +143,94 @@ export default class Team extends Component {
                 />
               </div>
               <p className="name">Prof. Vivek Pancholi</p>
-              <p className="des">Coordinator - METES</p> 
+              <p className="des">Coordinator - METES</p>
             </div>
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
           </div>
-        
-        
-        
-        {/*}
+
           <p className="team_type"> Student Body</p>
 
-    */}
+          <div className="team_array">
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Anant}
+                  alt="Anant Vashistha"
+                />
+              </div>
+              <p className = "name">Anant Vashistha</p>
+              <p className = "des">Overall Coordinator</p>
+            </div>
+{/*
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Anant}
+                  alt="Ankit Alok Bagaria"
+                />
+              </div>
+              <p className = "name">Ankit Alok Bagaria</p>
+              <p className = "des">Gen. Sec. - Technical Affairs</p>
+            </div>
+
+
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Anant}
+                  alt="Harshit Sharma"
+                />
+              </div>
+              <p className = "name">Harshit Sharma</p>
+              <p className = "des">Deputy Gen. Sec. - Technical Affairs</p>
+            </div>
+*/}
+
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Atul}
+                  alt="Atul Patidar"
+                />
+              </div>
+              <p className = "name">Atul Patidar</p>
+              <p className = "des">Head - Events & Promotions</p>
+            </div>
+
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Madhur}
+                  alt="Madhur Bhutke"
+                />
+              </div>
+              <p className = "name">Madhur Bhutke</p>
+              <p className = "des">Head - Sponsorship</p>
+            </div>
+
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Prasanna}
+                  alt="Prasannadeep Das"
+                />
+              </div>
+              <p className = "name">Prasannadeep Das</p>
+              <p className = "des">Head - Design & Web-D</p>
+            </div>
+
+            <div className="team_unit">
+              <div>
+                <img
+                  src={Marcha}
+                  alt="Sanjeevani Marcha"
+                />
+              </div>
+              <p className = "name">Sanjeevani Marcha</p>
+              <p className = "des">Head - Editorial</p>
+            </div>
+
+
+          </div>
         </div>
       </>
     );
