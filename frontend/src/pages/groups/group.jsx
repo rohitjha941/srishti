f1c6b1fdc57652c179aef961065ea4d21af92f26
Rohit <rjha@ph.iitr.ac.in>
2020-03-11 18:25:00 +0530

    Lint & Remove old code

--- frontend/src/pages/groups/group.jsx
+++ frontend/src/pages/groups/group.jsx
@@ -3,51 +3,44 @@ import "./group.scss";
 import { backend_url } from "../../const";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
+import Title from "../../components/title/title";
+import Text from "../../components/text/text";
 export default class Groups extends Component {
   state = {
-    groups: [],
+    groups: []
   };
   componentDidMount() {
     axios.get(backend_url + "/backend/group/").then(res => {
       this.setState({ groups: res.data });
-
     });
   }
   render() {
     return (
-      <div className='groups-wrapper'>
-        <h1>Our Groups</h1>
-        <h3>Following are listed the campus groups that are going to takepart in Srishti 2020</h3>
+      <div className="groups-wrapper">
+        <Title>Our Groups</Title>
+        <Text>
+          Following are listed the campus groups that are going to takepart in
+          Srishti 2020
+        </Text>
         <div className="groups">
           {this.state.groups.map(update => {
             return (
-              <NavLink to={{ pathname: "/groups/details", state: { groups: update } }}
-              >
-              <div key={update.id} className="groups-unit">
-                {/* <NavLink
+              <NavLink
                 to={{ pathname: "/groups/details", state: { groups: update } }}
               >
-                <p className="group-name">{update.name}</p>
-                <div className="group-image-holder">
-                  <img src={update.card_image} alt={update.name} />
-                </div>
-              </NavLink> */}
-
+                <div key={update.id} className="groups-unit">
                   <div className="group-image-holder">
                     <img src={update.card_image} alt={update.name} />
                   </div>
-                  <div className='groups-data-holder'>
-                    <div className='groups-header'>
-                      <p className='groups-name'>{update.name}</p>
-                      <img src={update.logo} width='4rem' alt={update.name} />
+                  <div className="groups-data-holder">
+                    <div className="groups-header">
+                      <p className="groups-name">{update.name}</p>
+                      <img src={update.logo} width="4rem" alt={update.name} />
                     </div>
-                    <div className='groups-info'>
-                      {update.description}
-                    </div>
+                    <div className="groups-info">{update.description}</div>
                   </div>
-              </div>
+                </div>
               </NavLink>
-
             );
           })}
         </div>
--- frontend/src/pages/groups_details/index.jsx
+++ frontend/src/pages/groups_details/index.jsx
@@ -12,48 +12,19 @@ export default class GoupDetails extends Component {
     console.log(data);
     return (
       <div className="group-details">
-        {/* <div className="group-details-image">
-          <img src={data.image} alt={data.name} />
-
-          <div className="group-details-logo">
-            <img src={data.logo} alt={data.name + "logo"} />
-          </div>
-        </div> */}
-
         <div className="groups-unit1">
-          {/* <NavLink
-                to={{ pathname: "/groups/details", state: { groups: data } }}
-              >
-                <p className="group-name">{data.name}</p>
-                <div className="group-image-holder">
-                  <img src={data.card_image} alt={data.name} />
-                </div>
-              </NavLink> */}
-
-
-          <div className='groups-data-holder1'>
-            <div className='groups-header1'>
-              <img src={data.logo} width='4rem' alt={data.name} />
-              <p className='groups-name1'>{data.name}</p>
+          <div className="groups-data-holder1">
+            <div className="groups-header1">
+              <img src={data.logo} width="4rem" alt={data.name} />
+              <p className="groups-name1">{data.name}</p>
             </div>
-            <div className='groups-info1'>
-              {data.description}
-            </div>
+            <div className="groups-info1">{data.description}</div>
           </div>
           <div className="group-image-holder1">
             <img src={data.card_image} alt={data.name} />
           </div>
         </div>
         <div className="group-details-inner">
-          {/* <div className="title2">{data.name}</div>
-          <span className="group-details-website">
-
-            <a target="_blank" rel="noopener noreferrer" href={data.web_url}>
-
-              Visit Website <img src={Line} alt="" />
-            </a>
-          </span>
-          <div className="group-details-description">{data.description}</div> */}
           {data.header.map(e => {
             return (
               <div className="group-header">
@@ -69,7 +40,6 @@ export default class GoupDetails extends Component {
             <div className="group-details-events">
               <div className="title2">Events that we we’ll organize </div>
 
-
               <div>
                 <table>
                   <tr>
@@ -81,48 +51,15 @@ export default class GoupDetails extends Component {
                   {data.events.map(e => {
                     return (
                       <tr>
-                        <td>
-                          {e.event_name}
-                        </td>
-                        <td>
-                          {e.date}
-                        </td>
-                        <td>
-                          {e.location}
-                        </td>
-                        <td>
-                          {e.time}
-                        </td>
-
+                        <td>{e.event_name}</td>
+                        <td>{e.date}</td>
+                        <td>{e.location}</td>
+                        <td>{e.time}</td>
                       </tr>
-
-                    )
-
-
-
+                    );
                   })}
                 </table>
               </div>
-              {/* // <div key={e.id} className="group-details-events-unit">
-                  //   <div className="group-details-events-image">
-                  //     <img src={e.event_image} alt={e.event_name} />
-                  //   </div>
-
-                  //   <div className="group-details-events-details">
-                  //     <div className="group-details-events-name">
-                  //       {e.event_name}
-                  //     </div>
-
-                  //     <div className="group-details-events-descritpion">
-                  //       {e.event_description}
-                  //     </div>
-
-                  //     <div className="group-details-events-location">
-                  //       {e.date} | {e.time} | {e.location}
-                  //     </div>
-                  //   </div>
-                  // </div> */}
-
             </div>
           )}
           {data.projects.length === 0 ? null : (
@@ -161,7 +98,7 @@ export default class GoupDetails extends Component {
                     </div>
 
                     <div className="group-contact-detail">
-                    <div className="group-header-title">Contact Us</div>
+                      <div className="group-header-title">Contact Us</div>
                       <div className="group-contact-detail-name">
                         {e.student_name}
                       </div>
@@ -173,7 +110,7 @@ export default class GoupDetails extends Component {
                         {e.email}
                       </div>
                       <div className="group-details-contact-designation">
-                       {e.phone}
+                        {e.phone}
                       </div>
                     </div>
                   </div>
